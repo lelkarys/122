56eaf7c23bfe79e0ccddb6d74cb438ced6d9d839
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-07-06 12:20:44 +0300

    ОН

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, isROwner, text }) => {
 
 handler.help = ['restart']
 handler.tags = ['owner']
-handler.command = /^(перезагрузить)?)$/i
+handler.command = /^(перезагрузить)$/i
 
 handler.rowner = true
 handler.register = true
