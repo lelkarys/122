b26a7c45a83fa65816113ac7e96dac1e55ad0152
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-07-06 14:54:36 +0300

    лшр

--- plugins/Anti-toxic.js
+++ plugins/Anti-toxic.js
@@ -1,4 +1,4 @@
-const toxicRegex = /puto|puta|rata|estupido|imbecil|rctmre|mrd|verga|vrga|maricon|puto|whore|rat|stupid|idiot|rctmre|mrd|dick|vrga|faggot/i
+const toxicRegex = /сука|блять|заебал|пидор|питух|нахуй|гандон|залупа|чмо|ебал|хуйло|пизда|шлюха|шалава|уёбище/i
 
 export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
     if (m.isBaileys && m.fromMe)
@@ -12,13 +12,13 @@ export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
     
     if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
        user.warn += 1
-       if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hola *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, decir la palabra (${isToxic}) está prohibido en este bot *${user.warn}/5* advertencia`, false, { mentions: [m.sender] })
-       if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hello *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, say the word (${isToxic}) is prohibited in this bot *${user.warn}/5* warning`, false, { mentions: [m.sender] })
+       if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Привет *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, сказать слово (${isToxic}) это запрещено в этом боте *${user.warn}/5* предупреждение`, false, { mentions: [m.sender] })
+       if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Здравствуйте *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, скажи это слово (${isToxic}) запрещено в этом боте *${user.warn}/5* предупреждение`, false, { mentions: [m.sender] })
      }
     
     if (user.warn >= 5) {
        user.warn = 0
-       await m.reply(`*ʜᴏʟᴀ*@${m.sender.split`@`[0]},* sᴜᴘᴇʀᴀsᴛᴇ ʟᴀs 5 ᴀᴅᴠᴇʀᴛᴇɴᴄɪᴀs sᴇʀᴀs ʙʟᴏǫᴜᴇᴀᴅᴏ ʏ ᴇʟɪᴍɪɴᴀᴅᴏ ᴅᴇ ᴇsᴛᴇ ɢʀᴜᴘᴏ*\n\n\n*HELLO*@${m.sender.split`@`[0]},*YOU EXCEEDED THE 5 WARNINGS YOU WILL BE BLOCKED AND REMOVED FROM THIS GROUP*`, false, { mentions: [m.sender] })
+       await m.reply(`*Привет*@${m.sender.split`@`[0]},* Вы превысили 5 предупреждений, вы будете заблокированы и удалены из этой группы*\n\n\n*HELLO*@${m.sender.split`@`[0]},*ВЫ ПРЕВЫСИЛИ 5 ПРЕДУПРЕЖДЕНИЙ, ВЫ БУДЕТЕ ЗАБЛОКИРОВАНЫ И УДАЛЕНЫ ИЗ ЭТОЙ ГРУППЫ*`, false, { mentions: [m.sender] })
        user.banned = true
        await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
        //await this.updateBlockStatus(m.sender, 'block')
