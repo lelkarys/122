b011cd89ee4347e3620c5e97b367e99e91bfb88a
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-07-07 22:52:46 +0300

    догнрп

--- plugins/Anti-privado.js
+++ plugins/Anti-privado.js
@@ -11,7 +11,7 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }
     let bot = global.db.data.settings[this.user.jid] || {}
     
     if (bot.antiPrivate && !isOwner && !isROwner) {
-       await m.reply(`*ʜᴏʟᴀ* @${m.sender.split`@`[0]}, *👋Извините, но это я запрещаю, приватный чат будет заблокирован🍀*\n\n\n*HELLO* @${m.sender.split`@`[0]}, *ИЗВИНИТЕ, НО ПРИВАТНОСТЬ ЗАПРЕЩЕНА 🍀*`, false, { mentions: [m.sender] })
+       await m.reply(`*ʜᴏʟᴀ* @${m.sender.split`@`[0]}, *👋Извините, но это я запрещаю, приватный чат будет заблокирован🍀*\n\n\n*`, false, { mentions: [m.sender] })
        await this.updateBlockStatus(m.chat, 'block')
     }
     return !1
