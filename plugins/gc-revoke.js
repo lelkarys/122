9818b4ef7d6a6fc60c536ee731c139eb87eda1c5
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-07-08 13:27:39 +0300

    зшгр

--- plugins/gc-revoke.js
+++ plugins/gc-revoke.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn }) => {
 let revoke = await conn.groupRevokeInvite(m.chat)
 await conn.reply(m.chat, `🔹️ *_Связь с группой была успешно восстановлена._*\n♾ • Link Nuevo: ${'https://chat.whatsapp.com/' + revoke}`, m)}
-handler.command = ['resetlink', 'revoke']
+handler.command = ['сброситьссылку', 'revoke']
 handler.botAdmin = true
 handler.admin = true
 handler.group = true
