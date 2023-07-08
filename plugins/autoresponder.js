a40707d6a1fc14d5a5499cbd711c736484c53d9e
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-07-09 00:08:10 +0300

    xgdhjm

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '⫷᭄©Ванилька﹏✍', body: 'Лёлька﹏✍', sourceUrl: `https://klike.net/uploads/posts/2023-02/1675590147_3-41.jpg`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: '⫷᭄©Ванилька﹏✍', body: 'Лёлька﹏✍', sourceUrl: `github.com/OFC-YOVANI/HADES-BOT-MD`, thumbnail: imagen2}}})}
     
 return !0 }
 handler.register = true
